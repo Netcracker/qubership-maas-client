a33b9063386dbbef73702ee22d1cbba9bbddd39a
Vladislav Larkin <larkin.qubership@gmail.com>
2026-03-13 12:08:30 +0400

    fix: use new KafkaContainer instead of deprecated

--- kafka-blue-green-consumer/src/test/java/com/netcracker/cloud/maas/bluegreen/kafka/impl/DefaultKafkaConsumerTest.java
+++ kafka-blue-green-consumer/src/test/java/com/netcracker/cloud/maas/bluegreen/kafka/impl/DefaultKafkaConsumerTest.java
@@ -52,7 +52,7 @@ class DefaultKafkaConsumerTest {
     static Properties producerProps;
 
     @Container
-    static KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0"));
+    static KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0").asCompatibleSubstituteFor("apache/kafka"));
 
     @BeforeAll
     static void setupKafka() {
--- kafka-streams-adapter/src/test/java/com/netcracker/cloud/maas/client/context/kafka/integration/KafkaStreamsIntTest.java
+++ kafka-streams-adapter/src/test/java/com/netcracker/cloud/maas/client/context/kafka/integration/KafkaStreamsIntTest.java
@@ -64,7 +64,7 @@ class KafkaStreamsIntTest {
     static String APP_ID = "words.count.app";
 
     @Container
-    static KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0"));
+    static KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0").asCompatibleSubstituteFor("apache/kafka"));
 
     static Admin admin;
     static String bootstrapServers;
